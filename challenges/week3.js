f68755a9530acdaef29eb6553a95167d4232a63e
Nichola Evans <nichola867@hotmail.com>
2019-11-24 20:46:38 +0000

    Task getTotalSubjects, checkIngredients and duplicateNumbers refactored

--- challenges/week3.js
+++ challenges/week3.js
@@ -10,59 +10,40 @@ function getSquares(nums) {
 //camel cases a single word (i.e. no capital letter at beginning)
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  let allWords = ((words).map((word) => word[0].toUpperCase() + word.slice(1))).join('');
+
+  let allWords = words.map(word => word[0].toUpperCase() + word.slice(1)).join('');
   return allWords[0].toLowerCase() + allWords.slice(1);
 }
-
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
 
   var result = people.map(n => n.subjects.length);
-  var sum = 0
-
-  for (var i = 0; i < people.length; i++) {
-    sum += result[i];
-  }
-  return sum;
+  return result.reduce(((total, n) => total += n), 0);
 }
 
-
 //returns false if no menu items include the specified ingredient"
-//REDO!!!
-
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
 
-  for (let i = 0; i < menu.length; i++) {
-    if (menu[i].ingredients.includes(ingredient)) {
-      return true;
-    }
-  }
-  return false;
+  return menu.filter((n) => n.ingredients.includes(ingredient)).length > 0;
 }
 
-//"returns an array of numbers which appear in both arr1 and arr2"
+
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
 
-  //find the commons numbers:
   let commonNums = arr1.filter(n => arr2.includes(n));
-
-  //pull out duplicates into a new array:
-  let noDups = Array.from(new Set(commonNums));
-
-  //return new array:
-  return noDups.sort();
+  return commonNums.filter((n, i) => commonNums.indexOf(n) === i).sort();
 
 
-  //commonNums.filter(n => commonNums.indexOf(n) !== n).sort();
+  // This also works but is less descriptive in how it functions:
+  // let noDups = Array.from(new Set(commonNums));
+  // return noDups.sort();
 }
-
-
 
 module.exports = {
   getSquares,
