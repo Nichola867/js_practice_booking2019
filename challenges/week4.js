2ccb500f51494945a895d275b30975a9eeb7e47e
Nichola Evans <nichola867@hotmail.com>
2019-11-26 18:26:18 +0000

    Task getSquareRoots completed

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,50 +1,54 @@
 //returns an array of numbers smaller than 1
-
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
- return nums.filter(n => n<1);
+
+  return nums.filter(n => n < 1);
 }
 
 //"returns an array of names beginning with the specified character"
-
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-return names.filter(n => n[0]===char);
+
+  return names.filter(n => n[0] === char);
 }
 
 
 //returns an array of words that are considered verbs (because they begin with 'to ')"
-
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-return words.filter(n => n.substring(0,3).match("to "));
+
+  return words.filter(n => n.substring(0, 3).match("to "));
 }
 
 
 //returns an array containing only integers"
-
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
+
   return nums.filter(n => n % 1 === 0);
 }
 
 
 //returns an array of the cities of each user"
-
 function getCities(users) {
   if (!users) throw new Error("users is required");
-let cities =  users.map(n => n.data.city.displayName);
-return cities;
+
+  let cities = users.map(n => n.data.city.displayName);
+  return cities;
 }
 
 
-
-
+//gets the square root of each number to 2 decimal places
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+
+  let srNums = nums.map(n => Math.sqrt(n).toFixed(2));
+  let decPlace = srNums.map(n => Math.round(n * 100) / 100);
+  return decPlace;
 }
+
+
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
--- test/week4.test.js
+++ test/week4.test.js
@@ -111,12 +111,12 @@ describe("getCities", () => {
   });
 });
 
-// describe("getSquareRoots", () => {
-//   test("gets the square root of each number to 2 decimal places", () => {
-//     const nums = [36, 77, 12, 355, 92, 5];
-//     expect(getSquareRoots(nums)).toEqual([6, 8.77, 3.46, 18.84, 9.59, 2.24]);
-//   });
-// });
+describe("getSquareRoots", () => {
+  test("gets the square root of each number to 2 decimal places", () => {
+    const nums = [36, 77, 12, 355, 92, 5];
+    expect(getSquareRoots(nums)).toEqual([6, 8.77, 3.46, 18.84, 9.59, 2.24]);
+  });
+});
 
 // describe("findSentencesContaining", () => {
 //   const sentencesAboutPackageJson = [
